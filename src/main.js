dedbd1cdeca031a7afa005554ff2e78001361d29
biggodw <biggodw@gmail.com>
2021-06-25 11:11:47 +0800

    demo init

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 引入全局样式
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
+// 创建Vue根实例
+// 并挂载相应配置
 new Vue({
   router,
   render: h => h(App)
